e1badc997ac6d980d9223c5195bc5580b4633233
Ana Eliza Bastos Martins de Freitas <ana.eliza@squadra.com.br>
2020-10-15 15:13:36 -0300

    Add project files.

---
+++ Models/IConta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financial.Models
+{
+    interface IConta
+    {
+        decimal VisualizarSaldo();
+        decimal GerarSaldo(decimal valor);
+    }
+}
